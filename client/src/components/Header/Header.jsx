09c79083327eeb9a3c81226daee7c955815f61f1
jairo <jairo.angulo93@gmail.com>
2020-12-10 17:27:02 -0500

    contact page created. title, form, button, and footer have been styled

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
 
   return (
     <div className="header-container">
+      
       <img
         id="home-img"
         src="/assets/foggy-mountains-nature-fog.jpg"
@@ -14,10 +15,10 @@ const Header = () => {
         <p>JA</p>
       </div>
       <ul className="links">
-        <li id="home-link">Home</li>
-        <li id="aboutme-link">About Me</li>
-        <li id="projects-link">Projects</li>
-        <li id="contactme-link">Contact Me</li>
+        <li id="home-link"><a href="#home-img">Home</a></li>
+        <li id="aboutme-link"><a href="#bio-title">About Me</a></li>
+        <li id="projects-link"><a href="#projects-section-title">Projects</a></li>
+        <li id="contactme-link"><a href="#contactme-title">Contact Me</a></li>
       </ul>
       <div className="short-bio">
         <img id="s-bio-img" src="/assets/366058cd421e6a981e50c6f800abbbd0.png" alt="Bio Image" />
--- client/src/screens/Contact Me/ContactMe.jsx
+++ client/src/screens/Contact Me/ContactMe.jsx
@@ -3,22 +3,36 @@ import './ContactMe.css'
 
 const ContactMe = () => {
 
-  const handleSubmit = (event) => {
-
-  }
-  
   return (
     <div className="contactme-box">
-      <h2>Want to work together?</h2>
-      <p>Contact me! Currently looking for new opportunities</p>
-      <form>
-
-      </form>
-      <div className="icons">
-        <a href="https://linkedin.com/in/jairo-angulo-232370139" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin-square"></a>
-        <a href="https://github.com/AnguloJM" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+      <div className="contactme-header">
+        <h2 id="contactme-title">Want to work together?</h2>
+        <p id="contactme-message">Contact me! Currently looking for new opportunities.</p>
       </div>
-      <p>Copyright &copy Jairo Angulo 2020</p>
+      <div className="contacts">
+        <form className="contactme-form" action="https://formspree.io/f/meqprzqg" method="post">
+          <div>
+            <label> Name:</label>
+            <input type="text" name="name" required/>
+          </div>
+          <div>
+            <label> Email:</label>
+            <input type="email" name="email" required/>
+          </div>
+          <div>
+            <label>Message:</label>
+            <textarea name="message" required></textarea>
+          </div>
+          <button type="submit" className="submit-button"><span>Send</span></button>
+        </form>
+      </div>
+      <div className="footer">
+        <div className="icons">
+          <a href="https://linkedin.com/in/jairo-angulo-232370139" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin-square"></a>
+          <a href="https://github.com/AnguloJM" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+        </div>
+        <p id="copyright">Copyright <i class="fa fa-copyright"></i> Jairo Angulo 2020</p>
+      </div>
     </div>
   );
 };
