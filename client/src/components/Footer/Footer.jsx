8f0218d5e2c5decd312ddbf1748ca479104c9f9e
jairo <jairo.angulo93@gmail.com>
2020-12-14 10:29:58 -0500

    filler images have been replaced

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
       <div className="icons">
         <a href="https://linkedin.com/in/jairo-angulo-232370139" target="_blank" rel="noopener noreferrer" className="fa fa-linkedin-square"></a>
         <a href="https://github.com/AnguloJM" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
+        <a href="https://drive.google.com/file/d/12_4erQrYcM5xZNDXXAIDQmIdGfgmmRFO/view?usp=sharing" target="_blank" rel="noopener noreferrer"><i className="fa fa-file-text" aria-hidden="true"></i></a>
       </div>
       <p id="copyright">Copyright <i class="fa fa-copyright"></i> Jairo Angulo 2020</p>
     </div>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,17 +10,8 @@ const Header = () => {
         src="/assets/foggy-mountains-nature-fog.jpg"
         alt="Mountain Image"
       />
-      {/* <div id="name-logo">
-        <p>JA</p>
-      </div>
-      <ul className="links">
-        <li id="home-link"><a href="#home-img">Home</a></li>
-        <li id="projects-link"><a href="#projects-section-title">Projects</a></li>
-        <li id="aboutme-link"><a href="#bio-title">About Me</a></li>
-        <li id="contactme-link"><a href="#contactme-title">Contact Me</a></li>
-      </ul> */}
       <div className="short-bio">
-        <img id="s-bio-img" src="/assets/366058cd421e6a981e50c6f800abbbd0.png" alt="Bio Image" />
+        <img id="s-bio-img" src="/assets/me.jpg" alt="Bio Image" />
         <p id="intro-name">Jairo Angulo</p>
         <p id="intro-statement">Aspiring Software-Engineer | Bringing Ideas to Life</p>
       </div>
